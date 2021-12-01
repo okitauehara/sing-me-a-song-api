3ee2ccdaac9d60c1c628319a74e83ddaed2199ee
okitauehara <marcosokita@live.com>
2021-12-01 13:52:52 -0300

    getRecommendations: findByScore implemented on repository

--- src/repositories/RecommendationRepository.js
+++ src/repositories/RecommendationRepository.js
@@ -15,6 +15,19 @@ class RecommendationRepository {
     return result.rows[0];
   }
 
+  async findByScore({ score }) {
+    if (score === 'good') {
+      const result = await connection.query(`
+      SELECT * FROM recommendations WHERE score > 10
+    `);
+      return result.rows;
+    }
+    const result = await connection.query(`
+      SELECT * FROM recommendations WHERE score <= 10
+    `);
+    return result.rows;
+  }
+
   async insert({ name, youtubeLink }) {
     const result = await connection.query(`
       INSERT INTO recommendations (name, "youtubeLink") VALUES ($1, $2) RETURNING *
