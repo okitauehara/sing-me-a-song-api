e427ba97ecbc852f28567c4f6eab78dc91ebef43
okitauehara <marcosokita@live.com>
2021-12-01 11:54:55 -0300

    postUpvote: postUpvote implemented on route

--- src/routes/recommendation.route.js
+++ src/routes/recommendation.route.js
@@ -4,5 +4,6 @@ import RecommendationController from '../controllers/RecommendationController.js
 const recommendationRouter = Router();
 
 recommendationRouter.post('/recommendations', RecommendationController.postRecommendation);
+recommendationRouter.post('/recommendations/:id/upvote', RecommendationController.postUpvote);
 
 export default recommendationRouter;
